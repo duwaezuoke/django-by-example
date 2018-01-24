6fd3eb9389410aa6ecbe260d482a566c8c39d584
duwaezuoke@unomaha.edu <duwaezuoke@unomaha.edu>
2018-01-23 21:07:52 -0600

    Django-by-Example Chapter 1 and 2 modified

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/', include('blog.urls',
-                           namespace='blog',
-                           app_name='blog')),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
 ]
